a1e1146cfeb5f514d3d93167258164cd2417e7ee
deergod <CommonTroll@live.co.uk>
2023-04-18 15:13:26 +0100

    filling out template function in controllers.js

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -2,7 +2,16 @@ const Book = require("./model");
 
 const addBook = async (req, res) => {
   try {
-    
+    const newBook = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre
+    });
+
+    res.status(201).json({
+      message: "Success",
+      newBook: newBook
+    });
   } catch (error) {
     console.log(error);
   }

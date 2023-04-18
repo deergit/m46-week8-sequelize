e924e1f15b322a3ee453c70c79d5a52764f0e1c0
deergod <CommonTroll@live.co.uk>
2023-04-18 13:59:07 +0100

    including app.use for express.json

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "App running correctly" });
 });

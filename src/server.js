6bd53ff1cfa39f520986afef69739389c3de41f4
deergod <CommonTroll@live.co.uk>
2023-04-18 14:26:30 +0100

    setting up connection js and using env file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const app = express();
+
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
@@ -10,4 +11,4 @@ app.get("/health", (req, res) => {
   res.status(200).json({ message: "App running correctly" });
 });
 
-app.listen(5001, () => console.log("Listen server open on port 5001"));
+app.listen(port, () => console.log(`Listen server open on port ${port}`));

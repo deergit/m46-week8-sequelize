9eca56ef0e972d5faf99d113bc3afab0b6124d18
deergod <CommonTroll@live.co.uk>
2023-04-18 16:03:59 +0100

    adding in all required functions and routes

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -13,10 +13,92 @@ const addBook = async (req, res) => {
       newBook: newBook
     });
   } catch (error) {
-    console.log(error);
+    res.status(501).json({
+      message: error.message
+    });
+  }
+}
+
+const getAllBooks = async (req, res) => {
+  try {
+    const bookList = await Book.findAll();
+
+    res.status(200).json({
+      message: "Success",
+      books: bookList
+    });
+  } catch (error) {
+    res.status(501).json({
+      message: error.message
+    });
+  }
+}
+
+const getBook = async (req, res) => {
+  try {
+    const book = await Book.findOne({ where: { title: req.params.title } });
+
+    res.status(200).json({
+      message: "Success",
+      book: book
+    });
+  } catch (error) {
+    res.status(501).json({
+      message: error.message
+    });
+  }
+}
+
+const updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.update({ author: req.body.newAuthor }, { where: { title: req.body.title } });
+
+    res.status(201).json({
+      message: "Success",
+      book: updatedBook
+    });
+  } catch (error) {
+    res.status(501).json({
+      message: error.message
+    });
+  }
+}
+
+const deleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.destroy({ where: { title: req.body.title } });
+
+    res.status(201).json({
+      message: "Success",
+      book: deletedBook
+    });
+  } catch (error) {
+    res.status(501).json({
+      message: error.message
+    });
+  }
+}
+
+const deleteAllBooks = async (req, res) => {
+  try {
+    const deletedBooks = await Book.destroy({ truncate: true });
+
+    res.status(201).json({
+      message: "Successfully deleted all books",
+      deletedBooks: deletedBooks
+    });
+  } catch (error) {
+    res.status(501).json({
+      message: error.message
+    });
   }
 }
 
 module.exports = {
   addBook,
+  getAllBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+  deleteAllBooks
 }
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
 const bookRouter = Router();
 
-const { addBook } = require("./controllers");
+const { addBook, getAllBooks, getBook, updateBook, deleteBook, deleteAllBooks } = require("./controllers");
 
 bookRouter.post("/books/addbook", addBook);
+
+bookRouter.get("/books/getallbooks", getAllBooks);
+
+bookRouter.get("/books/getbook/:title", getBook);
+
+bookRouter.put("/books/updatebook", updateBook);
+
+bookRouter.put("/books/deletebook", deleteBook);
+
+bookRouter.put("/books/deleteallbooks", deleteAllBooks);
 
 module.exports = bookRouter;

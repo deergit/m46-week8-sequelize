fb116636ca00e5a1ea55c3dd40807d53692a0cdf
deergod <CommonTroll@live.co.uk>
2023-04-18 14:47:13 +0100

    added book model

--- src/books/model.js
+++ src/books/model.js
@@ -1,6 +1,18 @@
 const { DataTypes } = require("sequelize");
-const connecton = require("../db/connection");
+const connection = require("../db/connection");
 
-const Book = connection.define();
+const Book = connection.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  genre: {
+    type: DataTypes.STRING
+  }
+});
 
 module.exports = Book;

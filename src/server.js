cd0a66eca07cfb7e8dc2276b2a3386c1a1904af2
deergod <CommonTroll@live.co.uk>
2023-04-18 13:53:37 +0100

    adding app healthcheck route

--- src/server.js
+++ src/server.js
@@ -4,4 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "App running correctly" });
+});
+
 app.listen(5001, () => console.log("Listen server open on port 5001"));

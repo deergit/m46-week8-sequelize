1640d40d1a909bbe54c8bd70ad4284260900ed4c
deergod <CommonTroll@live.co.uk>
2023-04-18 14:54:40 +0100

    added route hookup to server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const bookRouter = require("./books/routes");
 
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(bookRouter);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "App running correctly" });
